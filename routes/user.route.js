9b11e76c0223d60622c00996b4f9e5483235d037
Subhojit0012 <subhojitroy00012@gmail.com>
2025-04-14 21:42:01 +0530

    added the isLoggedIn validation middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,13 +5,13 @@ import {
   logoutUser,
   getAllUser,
 } from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
+import { authMiddleware, isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/allusers", getAllUser);
 router.post("/register", registerUser);
-router.post("/login", authMiddleware, loginUser);
+router.post("/login", isLoggedIn, loginUser);
 router.get("/logout", authMiddleware, logoutUser);
 
 export default router;
