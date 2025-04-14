8ef00bf8bd562c18240a31c3faf77136a7697f36
Subhojit0012 <subhojitroy00012@gmail.com>
2025-04-14 21:38:32 +0530

    feat: Enhance authentication middleware with isLoggedIn function and success response

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import ApiError from "../utils/apiError.js";
+import ApiSuccess from "../utils/apiSuccess.js";
 
 const prisma = new PrismaClient();
 
@@ -36,5 +37,31 @@ export const authMiddleware = async function (req, res, next) {
 
 // for checking if the user already login or not
 // we will create a function to check that the user are already logged in or not
+// retrive token from header (authorization)
+// check the user is exist or not
 
-export const isLoggedIn = (req, res) => {};
+// this is worked ✅
+
+export const isLoggedIn = async (req, res, next) => {
+  try {
+    const verificationToken = req.headers?.authorization.split(" ")[1];
+
+    // findUnique does't work because of ?(verificationToken?) but findFirst worked 
+    const isUserLoggedIn = await prisma.user.findFirst({
+      where: {
+        verificationToken,
+      },
+    });
+
+    if (!isUserLoggedIn) {
+      return res.json({ message: "user not found" });
+    }
+
+    isUserLoggedIn.isVerified = true;
+
+    console.log(ApiSuccess.create(200, "User logged in"));
+    next();
+  } catch (error) {
+    throw new ApiError(404, "Something went wrong!", error);
+  }
+};
