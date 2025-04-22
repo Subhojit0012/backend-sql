ac755f21ab0c496638cf8a7dbf1008ea8bb97a9f
Subhojit0012 <subhojitroy00012@gmail.com>
2025-04-22 21:56:30 +0530

    add password reset verification routes and update middleware imports

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,13 @@ import {
   loginUser,
   logoutUser,
   getAllUser,
+  passwordReset,
 } from "../controllers/auth.controller.js";
-import { authMiddleware, isLoggedIn } from "../middleware/auth.middleware.js";
+import {
+  authMiddleware,
+  isLoggedIn,
+  passwordResetEmail,
+} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -13,5 +18,7 @@ router.get("/allusers", getAllUser);
 router.post("/register", registerUser);
 router.post("/login", isLoggedIn, loginUser);
 router.get("/logout", authMiddleware, logoutUser);
+router.post("/verify", passwordResetEmail); // inside the handler -> next()
+router.post("/verify/change-password", passwordReset);
 
 export default router;
